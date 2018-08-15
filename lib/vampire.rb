e2a7366260c38155650e215ddf4490e1a63f0415
Brian Zanti <brianzanti@gmail.com>
2018-08-15 13:41:38 -0600

    updates vampire

---
+++ lib/vampire.rb
@@ -0,0 +1,28 @@
+class Vampire
+
+  attr_reader :name
+  attr_accessor :pet, :cape_color
+
+  def initalize (name, cape_color, pet="bat", thirstiness=true)
+    @name = name
+    @pet = pet
+    @thirstiness = thirstiness
+    @cape_color = cape_color
+  end
+
+  def thirsty
+    @thirstiness
+  end
+
+  def drink
+    @thirstiness = false
+  end
+
+  def sleep
+    true
+  end
+
+  def wake
+    @thirstiness = true
+  end
+end
