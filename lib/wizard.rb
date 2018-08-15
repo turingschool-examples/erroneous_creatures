c8e4b1b14ec5d6f642f621c291058f7918f8567c
Brian Zanti <brianzanti@gmail.com>
2018-08-15 13:46:38 -0600

    updates wizard

---
+++ lib/wizard.rb
@@ -0,0 +1,19 @@
+class Wizard
+
+  attr_reader :name
+  attr_accessor :bearded
+
+  def initialize(name, bearded = {bearded: true})
+    @name = name
+    @bearded = bearded
+  end
+
+  def bearded?
+    @bearded
+  end
+
+  def incantation(x)
+    "sudo #{x}"
+  end
+
+end
