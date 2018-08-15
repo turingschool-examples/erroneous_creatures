4fcf4dc4383b651caf8d558fbbef8d01b8d3b952
Brian Zanti <brianzanti@gmail.com>
2018-08-15 13:47:50 -0600

    updates medusa

---
+++ lib/medusa.rb
@@ -0,0 +1,36 @@
+class Medusa
+
+  attr_reader :name
+  attr_accessor :statues
+
+  def initialize(name)
+    @name = name
+    @statues = []
+  end
+
+  def stare(x)
+    x.stone = true
+    @statues = x
+  end
+
+  def statues
+    @statues
+  end
+
+end
+
+class Person
+
+  attr_reader :name
+  attr_accessor :stone
+
+  def initialize(name)
+    @name = name
+    @stone = false
+  end
+
+  def stoned?
+    @stone
+  end
+
+end
