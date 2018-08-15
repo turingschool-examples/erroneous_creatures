6c7561a29f5bedcea13639999b8fabed4a49b8f3
Brian Zanti <brianzanti@gmail.com>
2018-08-15 13:52:03 -0600

    updates hippogriff

---
+++ lib/hippogriff.rb
@@ -0,0 +1,37 @@
+class Hippogriff
+
+  attr_reader :name, :color
+  attr_accessor :moonrocks
+
+  def initialize(name, color)
+    @name = name
+    @color = color
+    moonrocks = []
+  end
+
+  def fly(rock)
+    rock.magicalness = true
+    @moonrocks.push(rock)
+  end
+
+  def sack
+    @moonrocks.to_h
+  end
+
+end
+
+class Moonstone
+
+  attr_reader :color
+  attr_accessor :magicalness
+
+  def initialize(color)
+    @color = color
+    @magicalness = false
+  end
+
+  def magic?
+    @magicalness
+  end
+
+end
