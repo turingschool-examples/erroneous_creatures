27c2ca3867cc074a5d04ed3139fb190dfb35c30b
Brian Zanti <brianzanti@gmail.com>
2018-08-15 14:42:21 -0600

    updates hobbit

--- lib/hobbit.rb
+++ lib/hobbit.rb
@@ -15,7 +15,7 @@ class Hobbit
   end
 
   def adult?
-    @age >= 32
+    @agee >= 32
   end
 
   def play
--- test/hobbit_test.rb
+++ test/hobbit_test.rb
@@ -5,26 +5,31 @@ require './lib/hobbit'
 
 class HobbitTest < Minitest::Test
   def test_it_has_a_name
+    skip
     hobbit = Hobbit.new("Bilbo")
     assert_equal "Bilbo", hobbit.name
   end
 
   def test_it_is_named_something_else
+    skip
     hobbit = Hobbit.new("Peregrin")
     assert_equal "Peregrin", hobbit.name
   end
 
   def test_disposition_is_unadventurous
+    skip
     hobbit = Hobbit.new("Samwise")
     assert_equal "homebody", hobbit.disposition
   end
 
   def test_can_have_a_different_disposition
+    skip
     hobbit = Hobbit.new("Frodo", "adventurous")
     assert_equal "adventurous", hobbit.disposition
   end
 
   def test_grows_older_when_celebrating_birthdays
+    skip
     hobbit = Hobbit.new('Meriadoc')
     assert_equal 0, hobbit.age
     5.times do
@@ -34,6 +39,7 @@ class HobbitTest < Minitest::Test
   end
 
   def test_is_considered_a_child_at_32
+    skip
     hobbit = Hobbit.new('Gerontius')
     32.times do
       hobbit.celebrate_birthday
@@ -42,6 +48,7 @@ class HobbitTest < Minitest::Test
   end
 
   def test_comes_of_age_at_33
+    skip
     hobbit = Hobbit.new('Otho')
     33.times do
       hobbit.celebrate_birthday
@@ -53,12 +60,8 @@ class HobbitTest < Minitest::Test
     assert hobbit.adult?
   end
 
-  def test_can_play_games
-    hobbit = Hobbit.new('Otho')
-    assert hobbit.respond_to?(:play)
-  end
-
   def test_can_only_play_games_if_a_child
+    skip
     hobbit = Hobbit.new('Otho')
     32.times do
       hobbit.celebrate_birthday
@@ -66,7 +69,7 @@ class HobbitTest < Minitest::Test
     assert_equal true, hobbit.play
   end
 
-  def can_get_tired_if_play_3times
+  def test_can_get_tired_if_play_3times
     hobbit = Hobbit.new('Otho')
     3.times do
       hobbit.play
