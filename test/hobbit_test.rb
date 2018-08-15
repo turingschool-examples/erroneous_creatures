3f830814fbfe77db14e4ef188fb77de726c46527
Brian Zanti <brianzanti@gmail.com>
2018-08-15 13:45:04 -0600

    fixes require

--- test/hobbit_test.rb
+++ test/hobbit_test.rb
@@ -1,7 +1,7 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative 'hobbit'
+require './lib/hobbit'
 
 class HobbitTest < Minitest::Test
   def test_it_has_a_name
@@ -73,5 +73,5 @@ class HobbitTest < Minitest::Test
     end
     assert_equal true, hobbit.tired?
   end
-  
+
 end
