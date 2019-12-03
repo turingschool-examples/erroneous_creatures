f7aafef74c8a8dccdbe8ac9968ffb08bc3c343e8
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2019-12-03 11:41:05 -0700

    Update unicorn_test.rb

--- test/unicorn_test.rb
+++ test/unicorn_test.rb
@@ -11,6 +11,7 @@ class UnicornTest < Minitest::Test
   end
 
   def test_it_is_white_by_default
+    skip
     unicorn = Unicorn.new("Margaret")
     assert_equal "white", unicorn.color
     assert_equal true, unicorn.white?
@@ -18,6 +19,7 @@ class UnicornTest < Minitest::Test
   end
 
   def test_it_does_not_have_to_be_white
+    skip
     unicorn = Unicorn.new("Barbara", "purple")
     assert_equal "purple", unicorn.color
     assert_equal false, unicorn.white?
@@ -25,22 +27,26 @@ class UnicornTest < Minitest::Test
   end
 
   def test_unicorn_says_sparkly_stuff
+    skip
     unicorn = Unicorn.new("Johnny")
     assert_equal "**;* Wonderful! **;*", unicorn.say("Wonderful!")
     assert_equal "**;* I don't like you very much. **;*", unicorn.say("I don't like you very much.")
   end
 
   def test_unicorn_can_fly
+    skip
     unicorn = Unicorn.new("Johnny")
     assert_equal true, unicorn.fly
   end
 
   def test_unicorn_can_eat_cotton_candy
+    skip
     unicorn = Unicorn.new("Johnny")
     assert_equal "**;* yummmm cotton candy **;*", unicorn.eat
   end
 
   def test_unicorn_cannot_fly_while_eating
+    skip
     unicorn = Unicorn.new("Johnny")
     unicorn.eat
     assert_equal false, unicorn.fly
