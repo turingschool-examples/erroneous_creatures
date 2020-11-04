7bac1f3dfd4a690ee145f84341703b01802f1691
memcmahon <mcmahon.meganelizabeth@gmail.com>
2020-11-04 16:22:37 -0700

    medusa first pass

---
+++ spec/medusa_spec.rb
@@ -0,0 +1,48 @@
+require './lib/medusa'
+
+RSpec.describe Medusa do
+  it "has a name" do
+    medusa = Medusa.new("Cassiopeia")
+
+    expect(medusa.name).to eq("Cassiopeia")
+  end
+
+  it "can stare" do
+    medusa = Medusa.new("Cassiopeia")
+
+    expect(medusa).to respond_to(:stare)
+  end
+
+  it "has statues" do
+    medusa = Medusa.new("Cassiopeia")
+
+    expect(medusa).to respond_to(:statues)
+  end
+
+  it "when first created she has no statues" do
+    medusa = Medusa.new("Cassiopeia")
+
+    expect(medusa.statues.empty?).to eq([])
+  end
+
+  it "when staring at a person she gains a statue" do
+    medusa = Medusa.new("Cassiopeia")
+    victim = Person.new("Perseus")
+
+    medusa.stare(victim)
+
+    expect(medusa.statues.count).to eq(1)
+    expect(medusa.statues.first.name).to eq("Perseus")
+  end
+
+  it "when staring at a person that person turns to stone" do
+    medusa = Medusa.new("Cassiopeia")
+    victim = Person.new("Perseus")
+
+    expect(victim.stoned?).to eq(false)
+
+    medusa.stare(victim)
+
+    expect(victim.stoned?).to eq(true)
+  end
+end
