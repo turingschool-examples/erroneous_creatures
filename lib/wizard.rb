99103d65c24ac2393a3a9c78cde3846336c7beb6
Candace Eckels <candace.eckels@gmail.com>
2023-02-14 17:28:54 -0700

    Wizards are bearded by default

--- lib/wizard.rb
+++ lib/wizard.rb
@@ -3,7 +3,7 @@ class Wizard
   attr_reader :name
   attr_accessor :bearded
 
-  def initialize(name, bearded = {bearded: true})
+  def initialize(name, bearded: true)
     @name = name
     @bearded = bearded
   end
